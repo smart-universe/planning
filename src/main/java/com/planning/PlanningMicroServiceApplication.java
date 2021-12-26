13403dbbe0dea432b150327364f69c0d57ec8add
bharat148vishnubhatla@gmail.com <=>
2021-12-26 14:38:58 +0530

    initial setup

---
+++ src/main/java/com/planning/PlanningMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package com.planning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanningMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanningMicroServiceApplication.class, args);
+	}
+
+}
